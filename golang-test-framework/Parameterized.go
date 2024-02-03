92bbf31f0671716898e0dc3315c2c74a110cbd97
cameron <cameron@bearstone.dev>
2024-02-03 08:24:52 -0800

    Extracted parameterized test constructs.

---
+++ golang-test-framework/Parameterized.go
@@ -0,0 +1,68 @@
+package golang_test_framework
+
+import (
+	"strconv"
+	"testing"
+)
+
+func createParameterized(t *testing.T) *parameterized {
+	return &parameterized{t, make([]scenario, 0)}
+}
+
+type parameterized struct {
+	*testing.T
+	scenarios []scenario
+}
+
+type scenario struct {
+	name *string
+	args []any
+}
+
+type Parameterized interface {
+	Scenario(...any) Parameterized
+	NamedScenario(string, ...any) Parameterized
+	Test(func(Assertions, []any))
+}
+
+func (p *parameterized) Scenario(args ...any) Parameterized {
+	p.scenarios = append(p.scenarios, scenario{nil, args})
+	return p
+}
+
+func (p *parameterized) NamedScenario(name string, args ...any) Parameterized {
+	p.scenarios = append(p.scenarios, scenario{&name, args})
+	return p
+}
+
+func (p *parameterized) Test(body func(Assertions, []any)) {
+	p.Helper()
+
+	parameterCount := 0
+	if len(p.scenarios) >= 1 {
+		parameterCount = len(p.scenarios[0].args)
+	} else {
+		p.Log("No scenarios to run.")
+		p.FailNow()
+	}
+
+	for testNumber, testScenario := range p.scenarios {
+		var name string
+		if testScenario.name == nil {
+			name = "Test #" + strconv.Itoa(testNumber)
+		} else {
+			name = *testScenario.name
+		}
+
+		p.Run(name, func(t *testing.T) {
+			testScenarioCopy := testScenario
+			asserts := createAssertions(name, t)
+			t.Parallel()
+			t.Helper()
+			asserts.True(func() bool {
+				return len(testScenarioCopy.args) == parameterCount
+			}, "Parameter count for scenario doesn't match prior scenarios.")
+			body(asserts, testScenarioCopy.args)
+		})
+	}
+}
--- golang-test-framework/Suite.go
+++ golang-test-framework/Suite.go
@@ -1,7 +1,6 @@
 package golang_test_framework
 
 import (
-	"strconv"
 	"testing"
 )
 
@@ -9,17 +8,17 @@ type suite struct {
 	*testing.T
 }
 
-func Tests(t *testing.T) Suite {
+func Tests(t *testing.T) TestSuite {
 	t.Parallel()
 	return &suite{t}
 }
 
-type Suite interface {
-	Test(string, func(Assertions)) Suite
+type TestSuite interface {
+	Test(string, func(Assertions)) TestSuite
 	Scenarios() Parameterized
 }
 
-func (s *suite) Test(name string, test func(Assertions)) Suite {
+func (s *suite) Test(name string, test func(Assertions)) TestSuite {
 	s.Run(name, func(t *testing.T) {
 		asserts := createAssertions(name, t)
 		t.Parallel()
@@ -30,63 +29,5 @@ func (s *suite) Test(name string, test func(Assertions)) Suite {
 }
 
 func (s *suite) Scenarios() Parameterized {
-	return &parameterized{s.T, make([]scenario, 0)}
-}
-
-type scenario struct {
-	name *string
-	args []any
-}
-
-type parameterized struct {
-	*testing.T
-	scenarios []scenario
-}
-
-type Parameterized interface {
-	Scenario(...any) Parameterized
-	NamedScenario(string, ...any) Parameterized
-	Test(func(Assertions, []any))
-}
-
-func (p *parameterized) Scenario(args ...any) Parameterized {
-	p.scenarios = append(p.scenarios, scenario{nil, args})
-	return p
-}
-
-func (p *parameterized) NamedScenario(name string, args ...any) Parameterized {
-	p.scenarios = append(p.scenarios, scenario{&name, args})
-	return p
-}
-
-func (p *parameterized) Test(body func(Assertions, []any)) {
-	p.Helper()
-
-	parameterCount := 0
-	if len(p.scenarios) >= 1 {
-		parameterCount = len(p.scenarios[0].args)
-	} else {
-		p.Log("No scenarios to run.")
-		p.FailNow()
-	}
-
-	for testNumber, testScenario := range p.scenarios {
-		var name string
-		if testScenario.name == nil {
-			name = "Test #" + strconv.Itoa(testNumber)
-		} else {
-			name = *testScenario.name
-		}
-
-		p.Run(name, func(t *testing.T) {
-			testScenarioCopy := testScenario
-			asserts := createAssertions(name, t)
-			t.Parallel()
-			t.Helper()
-			asserts.True(func() bool {
-				return len(testScenarioCopy.args) == parameterCount
-			}, "Parameter count for scenario doesn't match prior scenarios.")
-			body(asserts, testScenarioCopy.args)
-		})
-	}
+	return createParameterized(s.T)
 }
