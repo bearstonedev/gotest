90516389dfd3b9fb7342e6e52c2aee202ebfe121
cameron <cameron@bearstone.dev>
2024-02-18 14:26:30 -0800

    refactor: simplify assertion tests

--- assertions_test.go
+++ assertions_test.go
@@ -4,36 +4,23 @@ import (
 	"testing"
 )
 
-type TestableAssertions interface {
-	Assertions
-	createMockT() *tMock
-	getTestName() string
-}
-
-func (a *assertions) createMockT() *tMock {
-	mock := mockTesting(a.T)
-	a.T = mock
-	return mock
-}
-
-func (a *assertions) getTestName() string {
-	return a.testName
-}
+var _ Assertions = (*assertions)(nil)
 
 func TestAssertions_Inequality(t *testing.T) {
 	Tests(t).Scenarios().
 		Scenario("Two unequal numbers should be unequal", 1, 2).
 		Scenario("Two unequal strings should be unequal", "same", "different").
 		Scenario("Two unequal slices should be unequal", []string{"ラメン"}, []string{"寿司"}).
-		Test(func(asserts Assertions, scenario []any) {
-			sut := asserts.(TestableAssertions)
-			mm := sut.createMockT()
+		Test(func(sut Assertions, scenario []any) {
+			mockT, testName := setUpSystemUnderTest(sut)
 			lhs := scenario[0]
 			rhs := scenario[1]
+
 			sut.NotEqual(lhs, rhs)
-			mm.shouldNotFailTest()
+			mockT.shouldNotFailTest()
+
 			sut.Equal(lhs, rhs)
-			mm.shouldFailTest(sut.getTestName(), "Expected", lhs, "to be equal to", rhs)
+			mockT.shouldFailTest(testName, "Expected", lhs, "to be equal to", rhs)
 		})
 }
 
@@ -42,15 +29,16 @@ func TestAssertions_Equality(t *testing.T) {
 		Scenario("Two equal numbers should be equal", 1, 1).
 		Scenario("Two equal strings should be equal", "same", "same").
 		Scenario("Two equal slices should be equal", []string{"ラメン"}, []string{"ラメン"}).
-		Test(func(asserts Assertions, scenario []any) {
-			sut := asserts.(TestableAssertions)
-			mm := sut.createMockT()
+		Test(func(sut Assertions, scenario []any) {
+			mockT, testName := setUpSystemUnderTest(sut)
 			lhs := scenario[0]
 			rhs := scenario[1]
+
 			sut.Equal(lhs, rhs)
-			mm.shouldNotFailTest()
+			mockT.shouldNotFailTest()
+
 			sut.NotEqual(lhs, rhs)
-			mm.shouldFailTest(sut.getTestName(), "Expected", lhs, "not to be equal to", rhs)
+			mockT.shouldFailTest(testName, "Expected", lhs, "not to be equal to", rhs)
 		})
 }
 
@@ -58,20 +46,35 @@ func TestAssertions_TrueAndFalse(t *testing.T) {
 	Tests(t).Scenarios().
 		Scenario("true should be true", Assertions.True, true, "Expected true to be true").
 		Scenario("false should be false", Assertions.False, false, "Expected false to be false").
-		Test(func(asserts Assertions, scenario []any) {
-			sut := asserts.(TestableAssertions)
-			mm := sut.createMockT()
+		Test(func(sut Assertions, scenario []any) {
+			mockT, testName := setUpSystemUnderTest(sut)
 
 			assertionUnderTest := scenario[0].(func(Assertions, bool, ...any))
 			expected := scenario[1].(bool)
 			message := scenario[2].(string)
 
 			assertionUnderTest(sut, expected, message)
-			mm.shouldBeCalledTimes(1, "Helper")
-			mm.shouldNotFailTest()
+			mockT.shouldBeCalledTimes(1, "Helper")
+			mockT.shouldNotFailTest()
 
 			assertionUnderTest(sut, !expected, message)
-			mm.shouldBeCalledTimes(2, "Helper")
-			mm.shouldFailTest("Test", "\""+sut.getTestName()+"\"", "failed:", message)
+			mockT.shouldBeCalledTimes(2, "Helper")
+			mockT.shouldFailTest("Test", "\""+testName+"\"", "failed:", message)
 		})
+}
+
+func TestAssertions_Log(t *testing.T) {
+	Tests(t).Test("should pass logging calls", func(sut Assertions) {
+		mockT, _ := setUpSystemUnderTest(sut)
+		args := []any{"some", "stuff", "to", "log"}
+		sut.Log(args...)
+		mockT.shouldBeCalledWith("Log", args...)
+	})
+}
+
+func setUpSystemUnderTest(asserts Assertions) (*tMock, string) {
+	sut := asserts.(*assertions)
+	mockT := mockTesting(sut.T)
+	sut.T = mockT
+	return mockT, sut.testName
 }
