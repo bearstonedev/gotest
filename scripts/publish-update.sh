0c6e01575abc2305fd203677014344394e87db07
cameron <cameron@bearstone.dev>
2024-02-18 12:15:40 -0800

    ci: finish script for publishing new versions.

--- scripts/publish-update.sh
+++ scripts/publish-update.sh
@@ -1,4 +1,14 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+set -Eeuo pipefail
+
+if ((BASH_VERSINFO[0] < 4))
+then
+  echo "Sorry, you need at least bash-4.0 to run this script."
+  echo "Current version: ${BASH_VERSINFO[0]}"
+  exit 1
+fi
+
 
 changedFiles=$(git diff --cached --shortstat)
 if [[ -n $changedFiles ]]; then
@@ -6,13 +16,38 @@ if [[ -n $changedFiles ]]; then
   exit 1
 fi
 
-if [[ $# -ne 1 ]]; then
-    echo 'Version number is expected in the form: #.#.#'
-    echo 'The preceding v will be appended by the script.'
+if [[ ${#} -ne 1 ]]; then
+    echo 'Please provide the revision type as an argument (MAJOR, MINOR, PATCH).'
     exit 1
 fi
 
-versionNumber="v${1}"
-git tag "$versionNumber"
-git push origin "$versionNumber"
-GOPROXY=proxy.golang.org go list -m "github.com/bearstonedev/gotest@${versionNumber}"
+currentVersion=()
+IFS="." read -r -a currentVersion <<< "$(awk '{print $1}' scripts/published.version)"
+echo "Current version: v${currentVersion[0]}.${currentVersion[1]}.${currentVersion[2]}"
+
+newVersion=("${currentVersion[@]}")
+revision=${1^^}
+case ${revision} in
+  MAJOR|MINOR|PATCH) echo "Revision: ${revision}";;&
+  MAJOR) (( ++newVersion[0] ));;
+  MINOR) (( ++newVersion[1] ));;
+  PATCH) (( ++newVersion[2] ));;
+  *)
+    echo 'Must specify one of: MAJOR, MINOR, PATCH. Aborting update.'
+    exit 1
+    ;;
+esac
+
+newVersionFull="v${newVersion[0]}.${newVersion[1]}.${newVersion[2]}"
+echo "New version: ${newVersionFull}"
+
+echo "Creating a new tag for ${newVersionFull} ..."
+git tag "${newVersionFull}"
+git push origin "${newVersionFull}"
+
+echo "Updating Go proxy with ${newVersionFull} ..."
+GOPROXY=proxy.golang.org go list -m "github.com/bearstonedev/gotest@${newVersionFull}"
+
+echo "Updating published.version ..."
+echo -n "${newVersion[0]}.${newVersion[1]}.${newVersion[2]}" > scripts/published.version
+echo "Successfully published ${newVersionFull}!"
